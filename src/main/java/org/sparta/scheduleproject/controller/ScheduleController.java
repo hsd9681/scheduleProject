993b3fb3c5e8000461c52f02435b0ef28f126a12
hsd9681 <hsd9681@naver.com>
2024-05-20 16:37:01 +0900

    fix: 빌드 하나 추가

--- src/main/java/org/sparta/scheduleproject/controller/ScheduleController.java
+++ src/main/java/org/sparta/scheduleproject/controller/ScheduleController.java
@@ -1,11 +1,24 @@
 package org.sparta.scheduleproject.controller;
 
 import jakarta.validation.Valid;
+import org.sparta.scheduleproject.dto.FileUploadRequestDto;
 import org.sparta.scheduleproject.dto.ScheduleRequestDto;
 import org.sparta.scheduleproject.dto.ScheduleResponseDto;
 import org.sparta.scheduleproject.service.ScheduleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -21,6 +34,30 @@ public class ScheduleController {
     public ScheduleResponseDto createSchedule(@RequestBody @Valid ScheduleRequestDto requestDto){
         return scheduleService.createSchedule(requestDto);
     }
+    private final List<String> allowedExtensions = Arrays.asList("jpg", "jpeg", "png");
+
+    @PostMapping("/download")
+    public ResponseEntity<String> downloadImage(@RequestBody String imageUrl) {
+        try {
+            // 이미지 URL에서 이미지 다운로드
+            URL url = new URL(imageUrl);
+            InputStream inputStream = url.openStream();
+
+            // 이미지 저장 디렉토리 설정
+            String saveDirectory = System.getProperty("user.home") + File.separator + "downloaded_images" + File.separator;
+            Files.createDirectories(Paths.get(saveDirectory));
+
+            // 이미지 파일로 저장
+            Path imagePath = Paths.get(saveDirectory, "downloaded_image.jpg");
+            Files.copy(inputStream, imagePath);
+
+            return ResponseEntity.ok("Image downloaded and saved successfully to: " + imagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to download image: " + e.getMessage());
+        }
+    }
+
     //스케줄 하나 가져오기
     @GetMapping("/schedule/{id}")
     public List<ScheduleResponseDto> getoneSchedule(@PathVariable Long id) {
